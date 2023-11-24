6dafa3075c45376370754f1050f9acbf7ab3dc8c
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2023-11-24 17:23:46 +0200

    Remove search implementation

--- server/src/routes/books.js
+++ server/src/routes/books.js
@@ -30,56 +30,8 @@ routes.route('/search').get(async (req, res) => {
     return;
   }
 
-  const termEmbedding = await getEmbedding(term, process.env.EMBEDDING_ENDPOINT);
-  const records = await collection.aggregate([
-    {
-      $vectorSearch: {
-        index: 'textEmbeddings',
-        path: 'text_embedding',
-        queryVector: termEmbedding,
-        numCandidates: 100,
-        limit: 10 
-      }
-    },
-    {
-      $project: {
-        text_embedding: 0
-      }
-    },
-  ]).toArray();
-
-  return res.json(records);
-});
-
-routes.route('/search-image-by-text').get(async (req, res) => {
-  const term = req?.query?.term || '';
-  if (!term) {
-    res.status(400).send('Missing search term');
-    return;
-  }
-
-  const termEmbedding = await createMultiModalEmbedding({text: term});
-  console.log(termEmbedding);
-  const records = await vectorizedCollection.aggregate([
-    {
-      $vectorSearch: {
-        index: 'default',
-        path: 'embedding',
-        queryVector: termEmbedding,
-        numCandidates: 100,
-        limit: 10 
-      }
-    },
-    {
-      $project: {
-        embedding: 0
-      }
-    },
-  ]).toArray();
-
-  console.log(records);
-
-  return res.json(records);
+  // TODO: Implement sentiment search by term
+  res.send([]);
 });
 
 routes.route('/search-by-image').get(async (req, res) => {
@@ -89,26 +41,6 @@ routes.route('/search-by-image').get(async (req, res) => {
     return;
   }
 
-  const imageEmbedding = await createMultiModalEmbedding({imageURL: url});
-  console.log(imageEmbedding);
-  const records = await vectorizedCollection.aggregate([
-    {
-      $vectorSearch: {
-        index: 'imageEmbedding',
-        path: 'embedding',
-        queryVector: imageEmbedding,
-        numCandidates: 100,
-        limit: 10 
-      }
-    },
-    {
-      $project: {
-        embedding: 0
-      }
-    },
-  ]).toArray();
-
-  console.log(records);
-
-  return res.json(records);
+  // TODO: Implement search by image
+  res.send([]);
 });
